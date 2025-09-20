da545d11fa77d5edb3dcc2b9a1a8d25b1b9cb38e
cobets <cobets@gmail.com>
2025-09-20 18:27:04 +0000

    Enable Docker CUBEAI

--- docker/build.sh
+++ docker/build.sh
@@ -10,6 +10,6 @@ git -C src/micropython/ submodule update --init --depth=1
 
 # Build the firmware.
 make -j$(nproc) -C src/micropython/mpy-cross
-make -j$(nproc) TARGET=$TARGET -C src
+make -j$(nproc) TARGET=$TARGET CUBEAI=1 -C src
 mkdir -p ./docker/build/$TARGET
 cp -r src/build/bin/* ./docker/build/$TARGET
